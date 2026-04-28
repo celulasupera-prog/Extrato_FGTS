e4d32462ee7ed4930b08933816017f3609ef080b
celulasupera-prog <celulasupera@gmail.com>
2026-04-28 13:09:07 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -215,40 +215,17 @@ p, .stCaption, [data-testid="stCaptionContainer"] { color: var(--muted) !importa
   justify-content: center !important;
 }
  
-/* Container principal vira flex */
-[data-testid="stFileUploader"] section {
-  display: flex !important;
-  flex-direction: column !important;
-  align-items: center !important;
-  justify-content: center !important;
-  gap: 12px !important;
-}
-
-/* Wrapper interno também */
-[data-testid="stFileUploader"] section > div {
-  display: flex !important;
-  flex-direction: column !important;
-  align-items: center !important;
-  justify-content: center !important;
-  gap: 12px !important;
-}
-
-/* Nuvem primeiro */
-[data-testid="stFileUploader"] section > div::before {
-  content: "☁️";
-  font-size: 72px;
-  margin-bottom: 4px;
-}
-
-/* Botão depois */
+/* ORDENAÇÂO */
 [data-testid="stFileUploader"] button {
-  margin: 0 !important;
+  order: 1 !important;
 }
 
-/* Texto por último */
+[data-testid="stFileUploader"] section > div::before {
+  order: 2 !important;
+}
+
 [data-testid="stFileUploader"] section > div::after {
-  content: "Tamanho máximo: 200MB por arquivo • Formato: TXT";
-  margin-top: 6px;
+  order: 3 !important;
 }
  
 /* Remove TODOS os elementos nativos indesejados */
